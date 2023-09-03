a00d00464019c5e261bc1cc01802fe83dc83f0fa
aadityaps <aaditya.ps07@gmail.com>
2023-09-03 22:15:04 +0530

    Published the repo to Github

---
+++ Holidays Counter/index.js
@@ -0,0 +1,201 @@
+let count = 0
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    count = 0
+    countEl.textContent = count
+    
+}
+
+function decrement() {
+    count -= 1
+    countEl.textContent = count
+}
+
+
+let saveEle = document.getElementById("save-ele")
+let countEle = document.getElementById("count-ele")
+
+function incremente() {
+    count += 1
+    countEle.textContent = count
+}
+function saved() {
+    let countStr = count + " - "
+    saveEle.textContent += countStr
+    count = 0
+    countEle.textContent = count
+}
+function decremented() {
+    count -= 1
+    countEle.textContent = count
+}
+
+
+
+let saveEle3 = document.getElementById("save-ele3")
+let countEle3 = document.getElementById("count-ele3")
+
+function incremente3() {
+    count += 1
+    countEle3.textContent = count
+}
+function saved3() {
+    let countStr = count + " - "
+    saveEle3.textContent += countStr
+    count = 0
+    countEle3.textContent = count
+}
+
+function decremente3() {
+    count -= 1
+    countEle3.textContent = count
+}
+
+
+let saveEle4 = document.getElementById("save-ele4")
+let countEle4 = document.getElementById("count-ele4")
+
+function incremente4() {
+    count += 1
+    countEle4.textContent = count
+}
+function saved4() {
+    let countStr = count + " - "
+    saveEle4.textContent += countStr
+    count = 0
+    countEle4.textContent = count
+}
+
+function decremente4() {
+    count -= 1
+    countEle4.textContent = count
+}
+
+
+let saveEle5 = document.getElementById("save-ele5")
+let countEle5 = document.getElementById("count-ele5")
+
+function incremente5() {
+    count += 1
+    countEle5.textContent = count
+}
+function saved5() {
+    let countStr = count + " - "
+    saveEle5.textContent += countStr
+    count = 0
+    countEle5.textContent = count
+}
+
+function decremente5() {
+    count -= 1
+    countEle5.textContent = count
+}
+
+
+let saveEle6 = document.getElementById("save-ele6")
+let countEle6 = document.getElementById("count-ele6")
+
+function incremente6() {
+    count += 1
+    countEle6.textContent = count
+}
+
+function saved6() {
+    let countStr = count + " - "
+    saveEle6.textContent += countStr
+    count = 0
+    countEle6.textContent = count
+}
+
+function decremente6() {
+    count -= 1
+    countEle6.textContent = count
+}
+
+let saveEle7 = document.getElementById("save-ele7")
+let countEle7 = document.getElementById("count-ele7")
+
+function incremente7() {
+    count += 1
+    countEle7.textContent = count
+}
+function saved7() {
+    let countStr = count + " - "
+    saveEle7.textContent += countStr
+    count = 0
+    countEle7.textContent = count
+}
+
+function decremente7() {
+    count -= 1
+    countEle7.textContent = count
+}
+
+
+let saveEle8 = document.getElementById("save-ele8")
+let countEle8 = document.getElementById("count-ele8")
+
+function incremente8() {
+    count += 1
+    countEle8.textContent = count
+}
+function saved8() {
+    let countStr = count + " - "
+    saveEle8.textContent += countStr
+    count = 0
+    countEle8.textContent = count
+}
+
+function decremente8() {
+    count -= 1
+    countEle8.textContent = count
+}
+
+
+let saveEle9 = document.getElementById("save-ele9")
+let countEle9 = document.getElementById("count-ele9")
+
+function incremente9() {
+    count += 1
+    countEle9.textContent = count
+}
+function saved9() {
+    let countStr = count + " - "
+    saveEle9.textContent += countStr
+    count = 0
+    countEle9.textContent = count
+}
+
+function decremente9() {
+    count -= 1
+    countEle9.textContent = count
+}
+
+
+let saveEle10 = document.getElementById("save-ele10")
+let countEle10 = document.getElementById("count-ele10")
+
+function incremente10() {
+    count += 1
+    countEle10.textContent = count
+}
+function saved10() {
+    let countStr = count + " - "
+    saveEle10.textContent += countStr
+    count = 0
+    countEle10.textContent = count
+}
+
+function decremente10() {
+    count -= 1
+    countEle10.textContent = count
+}
